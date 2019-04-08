5caf59c8d4f70e577548a40c9ab06d72f31f1bf1
Sarpanchsaabji <46968261+Sarpanchsaabji@users.noreply.github.com>
2019-04-08 13:44:43 -0400

    Created new Method

--- week3.cs
+++ week3.cs
@@ -21,7 +21,16 @@ namespace ConsoleApp1
 
         public void newMethod()
         {
-	    		
+	    		int i = 0, sum = 0;
+		while(i < 5){
+			int num;
+			Console.WriteLine("Enter any number to add");
+			num = int.Parse(Console.ReadLine());
+			sum += num;
+			i++;
+		}
+	    Console.WriteLine("The sum of five number is {0}", sum);
+            Console.ReadLine();
         }
         
     }
